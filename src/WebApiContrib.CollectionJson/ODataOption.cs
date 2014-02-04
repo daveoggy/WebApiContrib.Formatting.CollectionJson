8324250163b2fec1260b6cc02a007f9229360cfc
Dave Ogden <ogdend@advisory.com>
2014-02-04 09:34:59 -0600

    Create new list of string in ODataOptions constructor.

--- src/WebApiContrib.CollectionJson/ODataOption.cs
+++ src/WebApiContrib.CollectionJson/ODataOption.cs
@@ -4,6 +4,11 @@ namespace WebApiContrib.CollectionJson
 {
     public class ODataOption
     {
+        public ODataOption()
+        {
+            Operations = new List<string>();
+        }
+
         public string Option { get; set; }
         public bool Enabled { get; set; }
         public IList<string> Operations { get; set; }
